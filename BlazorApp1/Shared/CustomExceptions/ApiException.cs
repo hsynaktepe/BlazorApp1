2040c1b1ac2bd962e073eb8a6f72f23e7e7f3bf9
hsynaktepe <56879020+hsynaktepe@users.noreply.github.com>
2022-10-11 16:18:20 +0300

    buggy giving a unauthorized

---
+++ BlazorApp1/Shared/CustomExceptions/ApiException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Shared.CustomExceptions
+{
+    public class ApiException : Exception
+    {
+        public ApiException(String Message, Exception InnerException) : base(Message, InnerException)
+        {
+
+        }
+
+        public ApiException(String Message) : base(Message)
+        {
+
+        }
+
+    }
+}
